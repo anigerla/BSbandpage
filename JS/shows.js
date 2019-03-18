add74926169d1b8b89000035bdb29490900066e1
Regina <regina.zaripova@gmail.com>
2019-03-18 14:55:27 -0400

    changed images and small styling changes

--- JS/shows.js
+++ JS/shows.js
@@ -30,7 +30,7 @@ collectShowTimes.then((response) => {
         //a link to ticket website is created and appended to the parent table cell
         let tixBtnLink = document.createElement('a');
         showsTd3.appendChild(tixBtnLink);
-        tixBtnLink.setAttribute("href", "https://www1.ticketmaster.ca/walk-off-the-earth-toronto-ontario-02-15-2019/event/1000554AB8084DEF?site=10085&pageType=178230&nativePromo=3211&slot=1&campaign=732286&flight=7410029&nativeId=7769792&artistid=1692033&majorcatid=10001&minorcatid=1");
+        tixBtnLink.setAttribute("href", "https://www1.ticketmaster.ca/");
         //a button element is created and inserted inside of the previously made <a> tag
         let getTixbtn = document.createElement('button');
         tixBtnLink.appendChild(getTixbtn);
@@ -42,56 +42,3 @@ collectShowTimes.then((response) => {
         getTixbtn.innerHTML = "Get tickets";
     }
 });
-
-   // data.forEach(element => {
-    //     console.log(element.place);
-    // });
-
-//function that categorizes parts of the shows data, creates empty html elements for it and places each data type into its allocated container; also, adds class names so that css styling is applied
-// function commentDisplay(data) {
-    // for (let i = 0; i <= commentDisplay.length; i++) {
-    //     let userComments = document.getElementById('userComments');
-    //     // below creates a div that is a child of userComments but a parent to the 2 spans and a p elements.
-    //     //  < !--parent - child-- >
-    //     let commentDiv = document.createElement('div');
-    //     // children of the CommentDiv
-    //     let commentSpan = document.createElement('span');
-    //     let commentDate = document.createElement('span');
-    //     let commentP = document.createElement('p');
-    //     // below appends children to the parent containers
-    //     // userComments.appendChild(commentDiv);
-    //     commentDiv.appendChild(commentSpan);
-    //     commentDiv.appendChild(commentDate);
-    //     commentDiv.appendChild(commentP);
-    //     // below adds class namdes to the created p and span elements
-    //     commentDiv.classList.add('userComments2');
-    //     commentSpan.classList.add("commentSpan");
-    //     commentDate.classList.add("commentDate");
-    //     commentP.classList.add("commentP");
-    //     // takes information collected by the below function from the input fields and fills the empty elements
-    //     commentSpan.innerHTML = data[i].name;
-    //     commentDate.innerHTML = "LALALA";
-    //     commentP.innerHTML = data[i].comment;
-    // }
-// };
-
-//function that grabs data types (show date, place and location) from fetched data array and creates a new object
-//function addComment() {
-    // event.preventDefault();
-    // document.getElementById("inputForm").reset();
-    // let userName = document.querySelector('input[name=userName]').value;
-    // let uComment = document.querySelector('input[name=userComment]').value;
-
-    // let showDate = document.getElementById;
-    // let showPlace = document.getElementById;
-    // let showLocation = document.getElementById;
-
-    // let newComment = {
-    //     date: userName,
-    //     place: "timeAgo",
-    //     location: uComment
-    // }
-
-// caller of the function that displays comments on the page (lines 21-31)
-//     commentDisplay(newComment);
-// };
